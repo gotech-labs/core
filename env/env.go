da3250575f2064970384e434bc81c6ac370d596f
masashi-toda <toda414@gmail.com>
2022-05-19 23:22:19 +0900

    add env package

---
+++ env/env.go
@@ -0,0 +1,66 @@
+package env
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gotech-labs/core/log"
+)
+
+// SetenvIfNotExists is ...
+func SetenvIfNotExists(key, value string) {
+	if os.Getenv(key) == "" {
+		os.Setenv(key, value)
+	}
+}
+
+// GetString is ...
+func GetString(key, fallback string) string {
+	v := os.Getenv(key)
+	if len(v) > 0 {
+		return v
+	}
+	return fallback
+}
+
+// GetInt is ...
+func GetInt(key string, fallback int) int {
+	value := GetString(key, "")
+	if len(value) > 0 {
+		if num, err := strconv.Atoi(value); err == nil {
+			return num
+		}
+	}
+	return fallback
+}
+
+// GetBool is ...
+func GetBool(key string, fallback bool) bool {
+	value := GetString(key, "")
+	switch strings.ToLower(value) {
+	case "true":
+		return true
+	default:
+		return fallback
+	}
+}
+
+// GetDuration is ...
+func GetDuration(key string, fallback time.Duration) (time.Duration, error) {
+	v := GetString(key, "")
+	if len(v) > 0 {
+		return time.ParseDuration(v)
+	}
+	return fallback, nil
+}
+
+// MustGetDuration is ...
+func MustGetDuration(key string, fallback time.Duration) time.Duration {
+	v, err := GetDuration(key, fallback)
+	if err != nil {
+		log.Panic().Err(err).Msgf("failed to parse duration [key=%s, value=%s]", key, os.Getenv(key))
+	}
+	return v
+}
